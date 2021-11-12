5abb10e0dcdcba9c176114939b5c452333af3b95
simanta <simanta.barua@yahoo.com>
2021-11-12 14:32:43 +0600

    firebase

--- src/Firebase/firebaseInit.js
+++ src/Firebase/firebaseInit.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebaseConfig";
 
-
+//initialize firebase
 const initializeFirebase = () => {
     initializeApp(firebaseConfig);
 }
