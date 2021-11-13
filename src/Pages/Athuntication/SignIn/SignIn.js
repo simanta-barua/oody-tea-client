e4a910923d52592d91f42e0d566278d5015e5479
simanta <simanta.barua@yahoo.com>
2021-11-13 14:03:01 +0600

    fix

--- src/Pages/Athuntication/SignIn/SignIn.js
+++ src/Pages/Athuntication/SignIn/SignIn.js
@@ -7,13 +7,8 @@ import { Button, Container, TextField, Typography, CircularProgress, Alert } fro
 import { useForm, Controller } from "react-hook-form";
 import { NavLink, useLocation, useHistory } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import './SingIn.css'
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
 
 const SignIn = () => {
     const [userData, setUserData] = useState({});
@@ -23,82 +18,75 @@ const SignIn = () => {
 
     const { control, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log(data);
         setUserData(data);
-        signInUser(user.email, user.password)
+        signInUser(data.email, data.password)
     }
 
     const handleGoogleSignIn = () => {
         signInWithGoogle(location, history)
     }
     return (
-        <Container>
+        <>
             <Box
                 component="form"
                 sx={{
-                    '& .MuiTextField-root': { my: 2, width: '70%', },
+                    mt: 5,
+                    '& .MuiTextField-root': { width: '100% ', py: 2 },
                 }}
                 autoComplete="off">
-                <Grid container spacing={2}>
-                    <Grid item sx={{ m: "auto" }} xs={6} md={8}>
-                        <Typography variant="h3" gutterBottom>Login
-                        </Typography>
-                        {!isLoading && <form  >
-                            <Controller
-                                name={"email"}
-                                control={control}
-                                rules={{
-                                    required: 'email is required',
-                                    pattern: {
-                                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                        message: 'Please enter a valid email',
-                                    }
-                                }}
-                                render={({ field }) => (
-                                    <TextField
-                                        {...field}
-                                        variant="standard"
-                                        label={"Email"}
-                                        type="text"
-                                        error={Boolean(errors.email)}
-                                        helperText={errors.email?.message}
-                                    />)} />
-                            <Controller
-                                name={"password"}
-                                control={control}
-                                rules={{
-                                    required: "password can't be empty",
-                                    minLength: {
-                                        value: 8,
-                                        message: "Password must have at least 8 characters"
-                                    }
-                                }}
-                                render={({ field }) => (
-                                    <TextField
-                                        {...field}
-                                        variant="standard"
-                                        label={"Password"}
-                                        type="password"
-                                        error={Boolean(errors.password)}
-                                        helperText={errors.password?.message} />
-                                )} />
-                            <Button sx={{ width: "70%" }} onClick={handleSubmit(onSubmit)} variant="contained">Login</Button>
-                            <p>or</p>
-                            <Button sx={{ mb: 2, width: "70%" }} onClick={handleGoogleSignIn} variant="contained">  Sign With google</Button>
-                            <NavLink to="/register"> <Button variant="contained">Not have an account? Register Here</Button></NavLink>
-                        </form>}
-                        {
-                            isLoading && <CircularProgress />
-                        }
-                        {user?.email && <Alert severity="success">Login successfully!</Alert>}
-                        {authError && <Alert severity="error">{authError}</Alert>}
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <img alt="" />
-                    </Grid>
-                </Grid>
+
+                <Typography variant="h3" gutterBottom> Sign In
+                </Typography>
+                {!isLoading && <form >
+                    <Controller
+                        name={"email"}
+                        control={control}
+                        rules={{
+                            required: 'email is required',
+                            pattern: {
+                                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                message: 'Please enter a valid email',
+                            }
+                        }}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                variant="standard"
+                                label={"Email"}
+                                type="text"
+                                error={Boolean(errors.email)}
+                                helperText={errors.email?.message}
+                            />)} />
+                    <Controller
+                        name={"password"}
+                        control={control}
+                        rules={{
+                            required: "password can't be empty",
+                            minLength: {
+                                value: 8,
+                                message: "Password must have at least 8 characters"
+                            }
+                        }}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                variant="standard"
+                                label={"Password"}
+                                type="password"
+                                error={Boolean(errors.password)}
+                                helperText={errors.password?.message} />
+                        )} />
+                    <Button sx={{ my: 2, width: "100%" }} onClick={handleSubmit(onSubmit)} variant="contained">Login</Button>
+                    <Button sx={{ my: 2, width: "100%" }} onClick={handleGoogleSignIn} variant="contained">  Sign With google</Button>
+                    <NavLink to="/register"> <Button >Not have an account? Register Here</Button></NavLink>
+                </form>}
+                {
+                    isLoading && <CircularProgress />
+                }
+                {user?.email && <Alert severity="success">Login successfully!</Alert>}
+                {authError && <Alert severity="error">{authError}</Alert>}
             </Box>
-        </Container>
+        </>
     );
 };
 
--- src/Pages/Dashboard/ManageOrder/ManageOrder.js
+++ src/Pages/Dashboard/ManageOrder/ManageOrder.js
@@ -6,21 +6,61 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
+import { Button, Select } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import useAuth from '../../../hooks/useAuth';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
 const ManageOrder = () => {
     const { user } = useAuth();
     const email = user?.email;
     const [myOrders, setMyOrders] = useState();
     console.log(myOrders);
 
+
     useEffect(() => {
         fetch(`http://localhost:5000/myOrders/${email}`)
             .then(res => res.json())
             .then(result => setMyOrders(result))
     }, []);
+    const [status, setStatus] = useState('');
 
+
+    const handleChange = (event) => {
+        setStatus(event.target.value);
+    };
+    console.log(status);
+
+    const handleDelete = (id) => {
+        console.log(id);
+        const proceed = window.confirm("Are you sure , you want to delete")
+        if (proceed) {
+            const url = `http://localhost:5000/deleteOrder/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount) {
+                        alert("Delete success");
+                        const remainingUser = myOrders.filter(user => user._id !== id);
+                        setMyOrders(remainingUser);
+                    }
+                });
+        }
+    }
+
+    const handleUpdate = (id) => {
+        fetch(`http://localhost:5000/updateStatus/${id})`, {
+            method: "PUT",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({ status }),
+        });
+        console.log(id);
+
+    }
     return (
         <>
             <TableContainer component={Paper}>
@@ -42,12 +82,30 @@ const ManageOrder = () => {
                                 <TableCell style={{ width: '20%' }} align="center" >{myOrder?.name}</TableCell>
                                 <TableCell align="right">{myOrder?.price}</TableCell>
                                 <TableCell style={{ width: '20%' }} align="right">{myOrder?.img}</TableCell>
-                                <TableCell align="center">{myOrder?.status}</TableCell>
                                 <TableCell align="center">
-                                    <Button variant="outlined" sx={{ m: 2 }} color="success" startIcon={<DeleteIcon />}>
-                                        Edit
+                                    <FormControl fullWidth>
+                                        <FormControl fullWidth>
+                                            <NativeSelect
+                                                onChange={handleChange}
+                                                defaultValue={myOrder?.status}
+                                                inputProps={{
+                                                    name: 'status',
+                                                    id: 'uncontrolled-native',
+
+                                                }}
+
+                                            >
+                                                <option value={"pending"}>Pending</option>
+                                                <option value={"approved"}>Approved</option>
+                                            </NativeSelect>
+                                        </FormControl>
+                                    </FormControl>
+                                </TableCell>
+                                <TableCell align="center">
+                                    <Button variant="outlined" sx={{ m: 2 }} onClick={() => handleUpdate(myOrder?._id)} color="success" startIcon={<DeleteIcon />}>
+                                        Update
                                     </Button>
-                                    <Button variant="outlined" sx={{ m: 2 }} color="error" startIcon={<DeleteIcon />}>
+                                    <Button variant="outlined" sx={{ m: 2 }} onClick={() => handleDelete(myOrder?._id)} color="error" startIcon={<DeleteIcon />}>
                                         Delete
                                     </Button>
                                 </TableCell>
